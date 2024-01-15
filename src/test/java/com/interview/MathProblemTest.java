32b738321edf8d71f553276aeebaa6f88721517d
Ker Tenepere <Ker10epere@gmail.com>
2024-01-15 16:12:51 +0800

    add junit dependency and MathProblemTest

---
+++ src/test/java/com/interview/MathProblemTest.java
@@ -0,0 +1,15 @@
+package com.interview;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MathProblemTest {
+
+    @Test
+    void secondMax() {
+    }
+
+    @Test
+    void secondMin() {
+    }
+}
