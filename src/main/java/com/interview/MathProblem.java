a432eaed665cdd1959d9f03fe0158594bb450929
Ker Tenepere <Ker10epere@gmail.com>
2024-01-15 16:10:27 +0800

    init commit

--- src/main/java/com/interview/MathProblem.java
+++ src/main/java/com/interview/MathProblem.java
@@ -1,4 +1,37 @@
 package com.interview;
 
+import java.util.List;
+
 public class MathProblem {
+    /*
+    Create a program given a list of numbers, returns second largest number.
+
+    given null list, returns null.
+    given empty list, returns null.
+    given list with size of 1, returns null.
+    given list with 2 items, returns second largest number.
+
+    Ex.
+    input -> nums = [5,4,1,2,3]
+    expected output -> 4, the second largest number
+     */
+    public Integer secondMax(List<Integer> nums) {
+        return null;
+    }
+
+    /*
+    Create a program that accepts list of numbers, and returns second smallest item.
+
+    given null list, returns null.
+    given empty list, returns null.
+    given list with size of 1, returns null.
+    given list with 2 items, returns second smallest number.
+
+    Ex.
+    input -> nums = [5,4,1,2,3]
+    expected output -> 2, the second largest number
+     */
+    public Integer secondMin(List<Integer> nums) {
+        return null;
+    }
 }
